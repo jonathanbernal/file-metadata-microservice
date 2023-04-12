31714a82cae58a06cece0b117c98751f8ac1d1e1
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-04-12 10:17:50 -0400

    Change boilerplate vars using var to const

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
