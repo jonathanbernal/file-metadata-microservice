bc5b9930dc779112bb51274444888dc4861dbdac
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-04-12 11:07:11 -0400

    Implement API code to read single file upload metadata

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
+const multer = require('multer'); //used for processing multipart/form-data
 require('dotenv').config()
 
 const app = express();
@@ -8,12 +8,24 @@ const app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// multer setup
+// since we only want to extract the metadata from the file,
+// we do not need to store it in local storage, but multer
+// still requires us to define a destination directory for
+// the uploads.
+const upload = multer({ dest: '/uploads' });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
